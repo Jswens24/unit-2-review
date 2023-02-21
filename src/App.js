a6a7d837777a008c1a548d68a92a65c17e906b3c
Jessica Swenson <jessicamswens@gmail.com>
2023-02-21 07:30:45 -0700

    added useeffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Mapper from './Mapper';
 
 function App() {
   const [chars, setChars] = useState([]);
 
   const fetchChars = () => {
     axios.get('https://swapi.dev/api/people')
-      .then(res => {
-        setChars(res.data)
+      .then((res) => {
+        setChars(res.data.results)
+        console.log(chars);
       })
+      .catch(err => console.log(err))
   }
+
+  useEffect(() => {
+    fetchChars();
+  }, [])
+
+  // useEffect(fetchChars, [])
+
+
 
   return (
     <div className="App">
-
+      <Mapper chars={chars} />
     </div>
   );
 }
